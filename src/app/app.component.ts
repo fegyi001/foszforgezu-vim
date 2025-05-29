01630c90918946076e32cf7485b8643927d36ac2
Gergely Padányi-Gulyás <work@gpadanyig.com>
2025-05-29 15:01:03 +0200

    asdf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent implements OnInit {
   map: Map | null = null
 
   ngOnInit() {
-    // OPTIMIZE: 3. Refactor this - move to a private method
+    // TODO: 3. Refactor this - move to a private method
     this.map = new Map({
       target: 'map',
       layers: [
